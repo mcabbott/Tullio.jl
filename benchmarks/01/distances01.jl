6211a22c618d1e72a447f006704197a4676b2ce1
Michael Abbott <me@escbook>
2020-04-27 12:22:00 +0200

    pull out cosines

--- benchmarks/01/distances01.jl
+++ benchmarks/01/distances01.jl
@@ -59,10 +59,13 @@ function distances_threaded_simd(data1, data2) # @baggepinnen
     lat2 = [deg2rad(data2[i,1]) for i in 1:size(data2, 1)]
     lng2 = [deg2rad(data2[i,2]) for i in 1:size(data2, 1)]
     # data = Matrix{Float64}(undef, length(lat1), length(lat2))
-    data = Matrix{eltype(data1)}(undef, length(lat1), length(lat2))
+    data = similar(data1, length(lat1), length(lat2))
+    cos_lat1 = @avx cos.(lat1)
     Threads.@threads for i in eachindex(lat2)
-        @inbounds lat, lng = lat2[i], lng2[i]
-        @avx data[:, i] .= @. sin((lat1 - lat) / 2)^2 + cos(lat1) * cos(lat) * sin((lng1 - lng) / 2)^2
+        # lat, lng = lat2[i], lng2[i]
+        @inbounds lat, cos_lat, lng = lat2[i], cos(lat2[i]), lng2[i]
+        # @avx data[:, i] .= @. sin((lat1 - lat) / 2)^2 + cos(lat1) * cos(lat) * sin((lng1 - lng) / 2)^2
+        @avx data[:, i] .= @. sin((lat1 - lat) / 2)^2 + cos_lat1 * cos_lat * sin((lng1 - lng) / 2)^2
     end
     Threads.@threads for i in eachindex(data)
         # @avx data[i] = 2.0 * 6373.0 * atan(sqrt(abs(data[i])), sqrt(abs(1.0 - data[i])))
@@ -109,8 +112,11 @@ function distances_einsum(data1deg, data2deg)
     data1 = deg2rad.(data1deg)
     data2 = deg2rad.(data2deg)
 
+    @einsum cd1[n] := cos(data1[n,1])
+    @einsum cd2[m] := cos(data2[m,1])
+
     @einsum data[n,m] := sin((data1[n,1] - data2[m,1])/2)^2 +
-        cos(data1[n,1]) * cos(data2[m,1]) * sin((data1[n,2] - data2[m,2])/2)^2
+        cd1[n] * cd2[m] * sin((data1[n,2] - data2[m,2])/2)^2
 
     @einsum data[n,m] = 2 * 6373 * atan(sqrt(abs(data[n,m])), sqrt(abs(1 - data[n,m])))
 end
@@ -119,8 +125,11 @@ function distances_vielsum(data1deg, data2deg)
     data1 = deg2rad.(data1deg)
     data2 = deg2rad.(data2deg)
 
+    @vielsum cd1[n] := cos(data1[n,1])
+    @vielsum cd2[m] := cos(data2[m,1])
+
     @vielsum data[n,m] := sin((data1[n,1] - data2[m,1])/2)^2 +
-        cos(data1[n,1]) * cos(data2[m,1]) * sin((data1[n,2] - data2[m,2])/2)^2
+        cd1[n] * cd2[m] * sin((data1[n,2] - data2[m,2])/2)^2
 
     @vielsum data[n,m] = 2 * 6373 * atan(sqrt(abs(data[n,m])), sqrt(abs(1 - data[n,m])))
 end
@@ -134,8 +143,11 @@ function distances_cast(data1deg, data2deg)
     data1 = deg2rad.(data1deg)
     data2 = deg2rad.(data2deg)
 
+    @cast cd1[n] := cos(data1[n,1]) # pulling these out is worth 25%
+    @cast cd2[m] := cos(data2[m,1])
+
     @cast data[n,m] := sin((data1[n,1] - data2[m,1])/2)^2 +
-        cos(data1[n,1]) * cos(data2[m,1]) * sin((data1[n,2] - data2[m,2])/2)^2
+        cd1[n] * cd2[m] * sin((data1[n,2] - data2[m,2])/2)^2
 
     @cast data[n,m] = 2 * 6373 * atan(sqrt(abs(data[n,m])), sqrt(abs(1 - data[n,m])))
 end
@@ -144,8 +156,11 @@ function distances_cast_avx(data1deg, data2deg)
     data1 = deg2rad.(data1deg)
     data2 = deg2rad.(data2deg)
 
+    @cast cd1[n] := cos(data1[n,1])  avx
+    @cast cd2[m] := cos(data2[m,1])  avx
+
     @cast data[n,m] := sin((data1[n,1] - data2[m,1])/2)^2 +
-        cos(data1[n,1]) * cos(data2[m,1]) * sin((data1[n,2] - data2[m,2])/2)^2  avx
+        cd1[n] * cd2[m] * sin((data1[n,2] - data2[m,2])/2)^2  avx
 
     @cast data[n,m] = 2 * 6373 * atan(sqrt(abs(data[n,m])), sqrt(abs(1 - data[n,m])))  avx
 end
@@ -165,6 +180,19 @@ function distances_tullio(data1deg, data2deg)
     @tullio data[n,m] = 2 * 6373 * atan(sqrt(abs(data[n,m])), sqrt(abs(1 - data[n,m])))
 end
 
+# function distances_tullio2(data1deg, data2deg)
+#     data1 = deg2rad.(data1deg)
+#     data2 = deg2rad.(data2deg)
+
+#     @tullio cd1[n] := cos(data1[n,1]) # has no effect
+#     @tullio cd2[m] := cos(data2[m,1])
+
+#     @tullio data[n,m] := sin((data1[n,1] - data2[m,1])/2)^2 +
+#         cd1[n] * cd2[m] * sin((data1[n,2] - data2[m,2])/2)^2
+
+#     @tullio data[n,m] = 2 * 6373 * atan(sqrt(abs(data[n,m])), sqrt(abs(1 - data[n,m])))
+# end
+
 @test res ≈ distances_tullio(a, b)
 @test eltype(distances_tullio(a, b)) == Float32
 
@@ -182,7 +210,7 @@ julia> @btime distances_threaded($a, $b);
   516.937 ms (64 allocations: 95.45 MiB)
 
 julia> @btime distances_threaded_simd($a, $b);
-  235.098 ms (64 allocations: 95.45 MiB)
+  215.938 ms (66 allocations: 95.47 MiB)
 
 julia> @btime distances_bcast($a, $b);
   1.352 s (10 allocations: 95.44 MiB)
@@ -191,52 +219,19 @@ julia> @btime distances_bcast_simd($a, $b);
   641.506 ms (43 allocations: 95.44 MiB)
 
 julia> @btime distances_einsum($a, $b);
-  1.435 s (6 allocations: 95.44 MiB)
+  983.168 ms (10 allocations: 95.48 MiB)
 
 julia> @btime distances_vielsum($a, $b);
-  572.038 ms (53 allocations: 95.45 MiB)
+  389.831 ms (103 allocations: 95.49 MiB)
 
-julia> @btime distances_cast($a, $b); # should be much like distances_bcast
-  1.339 s (12 allocations: 95.44 MiB)
+julia> @btime distances_cast($a, $b); # unlike distances_bcast, this pulls out cos(...)
+  1.034 s (16 allocations: 95.48 MiB)
 
-julia> @btime distances_cast_avx($a, $b); # applies @avx to two broadcasts, not one
-  170.653 ms (45 allocations: 190.81 MiB)
+julia> @btime distances_cast_avx($a, $b); # and this applies more @avx than bcast_simd
+  137.557 ms (43 allocations: 190.85 MiB)
 
 julia> @btime distances_tullio($a, $b);
   51.442 ms (636 allocations: 95.47 MiB)
-
-julia> a = -100 .+ 200 .* rand(Float64, 5000, 2); ##### repeat everythinng in Float64
-julia> b = -100 .+ 200 .* rand(Float64, 5000, 2);
-
-julia> @btime distances($a, $b);
-  1.716 s (26 allocations: 572.36 MiB)
-
-julia> @btime distances_threaded($a, $b);
-  550.213 ms (64 allocations: 190.89 MiB)
-
-julia> @btime distances_threaded_simd($a, $b);
-  348.886 ms (64 allocations: 190.89 MiB)
-
-julia> @btime distances_bcast($a, $b);
-  1.533 s (10 allocations: 190.89 MiB)
-
-julia> @btime distances_bcast_simd($a, $b);
-  981.204 ms (43 allocations: 190.89 MiB)
-
-julia> @btime distances_einsum($a, $b);
-  1.454 s (6 allocations: 190.89 MiB)
-
-julia> @btime distances_vielsum($a, $b);
-  549.669 ms (52 allocations: 190.89 MiB)
-
-julia> @btime distances_cast($a, $b);
-  1.546 s (12 allocations: 190.89 MiB)
-
-julia> @btime distances_cast_avx($a, $b);
-  607.745 ms (45 allocations: 381.62 MiB)
-
-julia> @btime distances_tullio($a, $b);
-  188.599 ms (636 allocations: 190.91 MiB)
 
 
 
@@ -246,34 +241,37 @@ julia> a = -100 .+ 200 .* rand(Float32, 5000, 2);
 julia> b = -100 .+ 200 .* rand(Float32, 5000, 2);
 
 julia> @btime distances($a, $b);
-  1.167 s (26 allocations: 286.18 MiB)
+  1.166 s (26 allocations: 286.18 MiB)
 
 julia> @btime distances_threaded($a, $b);
-  140.228 ms (144 allocations: 95.46 MiB)
+  140.062 ms (144 allocations: 95.46 MiB)
 
 julia> @btime distances_threaded_simd($a, $b);
-  65.031 ms (144 allocations: 95.46 MiB)
+  64.382 ms (147 allocations: 95.48 MiB)
 
 julia> @btime distances_bcast($a, $b);
   1.033 s (10 allocations: 95.44 MiB)
 
 julia> @btime distances_bcast_simd($a, $b);
-  501.345 ms (43 allocations: 95.44 MiB)
+  501.002 ms (43 allocations: 95.44 MiB)
 
 julia> @btime distances_einsum($a, $b);
-  993.101 ms (6 allocations: 95.44 MiB)
+  756.749 ms (10 allocations: 95.48 MiB)
 
 julia> @btime distances_vielsum($a, $b);
-  145.149 ms (133 allocations: 95.46 MiB)
+  108.200 ms (262 allocations: 95.51 MiB)
 
-julia> @btime distances_cast($a, $b); # should be much like distances_bcast
-  1.036 s (12 allocations: 95.44 MiB)
+julia> @btime distances_cast($a, $b); # unlike distances_bcast, this pulls out cos(...)
+  795.199 ms (16 allocations: 95.48 MiB)
 
-julia> @btime distances_cast_avx($a, $b); # applies @avx to two broadcasts, not one
-  134.320 ms (45 allocations: 190.81 MiB)
+julia> @btime distances_cast_avx($a, $b); # and this applies more @avx than bcast_simd
+  112.824 ms (43 allocations: 190.85 MiB)
 
 julia> @btime distances_tullio($a, $b);
-  27.329 ms (789 allocations: 95.48 MiB)
+  28.151 ms (788 allocations: 95.48 MiB)
+
+
+
 
 julia> a = -100 .+ 200 .* rand(Float64, 5000, 2); ##### repeat everythinng in Float64
 julia> b = -100 .+ 200 .* rand(Float64, 5000, 2);
@@ -282,31 +280,31 @@ julia> @btime distances($a, $b);
   1.308 s (26 allocations: 572.36 MiB)
 
 julia> @btime distances_threaded($a, $b);
-  146.515 ms (145 allocations: 190.90 MiB)
+  146.374 ms (144 allocations: 190.90 MiB)
 
 julia> @btime distances_threaded_simd($a, $b);
-  98.534 ms (144 allocations: 190.90 MiB)
+  92.097 ms (146 allocations: 190.94 MiB)
 
 julia> @btime distances_bcast($a, $b);
-  1.136 s (10 allocations: 190.89 MiB)
+  1.134 s (10 allocations: 190.89 MiB)
 
 julia> @btime distances_bcast_simd($a, $b);
-  729.348 ms (43 allocations: 190.89 MiB)
+  728.564 ms (43 allocations: 190.89 MiB)
 
 julia> @btime distances_einsum($a, $b);
-  1.087 s (6 allocations: 190.89 MiB)
+  874.312 ms (10 allocations: 190.96 MiB)
 
 julia> @btime distances_vielsum($a, $b);
-  149.393 ms (133 allocations: 190.90 MiB)
+  123.725 ms (262 allocations: 191.00 MiB)
 
 julia> @btime distances_cast($a, $b);
-  1.139 s (12 allocations: 190.89 MiB)
+  902.447 ms (16 allocations: 190.96 MiB)
 
 julia> @btime distances_cast_avx($a, $b);
-  473.684 ms (45 allocations: 381.62 MiB)
+  431.136 ms (43 allocations: 381.70 MiB)
 
 julia> @btime distances_tullio($a, $b);
-  75.990 ms (790 allocations: 190.93 MiB)
+  75.608 ms (786 allocations: 190.93 MiB)
 
 
 
@@ -327,12 +325,12 @@ julia> @test cres ≈ distances_cast(ca, cb)
 Test Passed
 
 julia> @btime CuArrays.@sync distances_bcast($ca, $cb);
-  31.550 ms (420 allocations: 18.42 KiB)
+  31.558 ms (420 allocations: 18.42 KiB)
 
 julia> @btime CuArrays.@sync distances_cast($ca, $cb);
-  31.663 ms (392 allocations: 17.94 KiB)
+  29.728 ms (546 allocations: 22.48 KiB)
 
 julia> @btime CuArrays.@sync distances_tullio($ca, $cb);
-  188.699 ms (330445 allocations: 5.05 MiB)
+  187.258 ms (173551 allocations: 2.66 MiB)
 
 
