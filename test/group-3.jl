313481965109c97c51afeccabd61fe0a0e703b33
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2021-04-26 17:59:20 -0400

    tidy

--- test/group-3.jl
+++ test/group-3.jl
@@ -153,20 +153,15 @@ end
 
 @testset "parsing + LoopVectorization" begin include("parsing.jl") end
 
-# if test_group != "3" # Github CI fails, on some runs, "ERROR: Package Tullio errored during testing (received signal: KILL)"
-    # https://github.com/mcabbott/Tullio.jl/pull/57/checks?check_run_id=1753332805
+using Tracker
+GRAD = :Tracker
+_gradient(x...) = Tracker.gradient(x...)
 
-    using Tracker
-    GRAD = :Tracker
-    _gradient(x...) = Tracker.gradient(x...)
+@tullio grad=Base
+@testset "gradients: Tracker + DiffRules + LoopVectorization" begin include("gradients.jl") end
 
-    @tullio grad=Base
-    @testset "gradients: Tracker + DiffRules + LoopVectorization" begin include("gradients.jl") end
-
-    @tullio grad=Dual
-    @testset "gradients: Tracker + ForwardDiff + LoopVectorization" begin include("gradients.jl") end
-
-# end
+@tullio grad=Dual
+@testset "gradients: Tracker + ForwardDiff + LoopVectorization" begin include("gradients.jl") end
 
 @info @sprintf("LoopVectorization tests took %.1f seconds", time()-t8)
 
