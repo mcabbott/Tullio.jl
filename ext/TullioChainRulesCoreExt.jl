52a1bd9e76dcb86f2d971669694ea96aff672884
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2025-01-23 13:47:29 -0500

    Allow Zygote v0.7 (#188)
    * Update TullioChainRulesCoreExt.jl
    
    * Update Project.toml

--- ext/TullioChainRulesCoreExt.jl
+++ ext/TullioChainRulesCoreExt.jl
@@ -6,7 +6,7 @@ function ChainRulesCore.rrule(ev::Tullio.Eval, args...)
     Z = ev.fwd(args...)
     Z, function tullio_back(Δ)
         isnothing(ev.rev) && error("no gradient definition here!")
-        dxs = map(ev.rev(Δ, Z, args...)) do dx
+        dxs = map(ev.rev(unthunk(Δ), Z, args...)) do dx
             dx === nothing ? ChainRulesCore.ZeroTangent() : dx
         end
         tuple(ChainRulesCore.ZeroTangent(), dxs...)
